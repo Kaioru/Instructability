c9af6523aeb63aa743ea89ac8904daef09698efa
Kaioru <keifuurawrs@gmail.com>
2016-05-02 13:48:54 +0800

    Add JavaDocs to CommandBuilder

--- src/main/java/instructabilty/command/CommandBuilder.java
+++ src/main/java/instructabilty/command/CommandBuilder.java
@@ -26,21 +26,45 @@ public class CommandBuilder implements Builder<BuiltCommand> {
 		this.allowPrivateMessage = false;
 	}
 
+	/**
+	 * Adds a description to the command
+	 *
+	 * @param desc
+	 * @return
+	 */
 	public CommandBuilder desc(String desc) {
 		this.desc = desc;
 		return this;
 	}
 
+	/**
+	 * Adds an alias to the command
+	 *
+	 * @param alias
+	 * @return
+	 */
 	public CommandBuilder alias(String alias) {
 		aliases.add(alias);
 		return this;
 	}
 
+	/**
+	 * Adds a sub-command to the command
+	 *
+	 * @param command
+	 * @return
+	 */
 	public CommandBuilder command(Command command) {
 		this.commands.add(command);
 		return this;
 	}
 
+	/**
+	 * Adds permission to the command
+	 *
+	 * @param permission
+	 * @return
+	 */
 	public CommandBuilder permission(String permission) {
 		if (permission.contains("."))
 			this.permission = new CommandPermission(permission);
@@ -49,26 +73,52 @@ public class CommandBuilder implements Builder<BuiltCommand> {
 		return this;
 	}
 
+	/**
+	 * Removes helper commands from the command
+	 *
+	 * @return CommandBuilder
+	 */
 	public CommandBuilder noHelperCommands() {
 		this.addHelperCommands = false;
 		return this;
 	}
 
+	/**
+	 * Removes the ability to remove the message used to trigger the command
+	 *
+	 * @return CommandBuilder
+	 */
 	public CommandBuilder noRemoveTrigger() {
 		this.removeTriggerMessage = false;
 		return this;
 	}
 
+	/**
+	 * Removes the ability to execute the command from a private channel
+	 *
+	 * @return CommandBuilder
+	 */
 	public CommandBuilder noPrivateMessage() {
 		this.allowPrivateMessage = false;
 		return this;
 	}
 
+	/**
+	 * Builds the command with the specified options
+	 *
+	 * @return BuiltCommand
+	 */
 	@Override
 	public BuiltCommand build() {
 		return build((event, msg, args) -> {});
 	}
 
+	/**
+	 * Builds the command with the specified options
+	 *
+	 * @param executable The command executable for the command
+	 * @return BuiltCommand
+	 */
 	public BuiltCommand build(CommandExecutable executable) {
 		return new BuiltCommand(name,
 				desc,
