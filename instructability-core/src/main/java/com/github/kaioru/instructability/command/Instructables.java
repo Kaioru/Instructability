73c6cb96a9a2ede6819cc6586a2d193b51798dc0
Kaioru <keifuurawrs@gmail.com>
2016-05-07 14:02:51 +0800

    Add logger

--- instructability-core/src/main/java/com/github/kaioru/instructability/command/Instructables.java
+++ instructability-core/src/main/java/com/github/kaioru/instructability/command/Instructables.java
@@ -1,6 +1,11 @@
 package com.github.kaioru.instructability.command;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Instructables {
+
+	public static Logger LOGGER = LoggerFactory.getLogger(Instructables.class);
 
 	private static CommandRegistry reg = new CommandRegistry();
 
