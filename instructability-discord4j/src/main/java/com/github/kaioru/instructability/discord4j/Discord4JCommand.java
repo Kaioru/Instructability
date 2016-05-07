58d303a32d1dbd21067dc241c6eab4a90021dcf3
Kaioru <keifuurawrs@gmail.com>
2016-05-07 13:36:52 +0800

    Re-add permissions

--- instructability-discord4j/src/main/java/com/github/kaioru/instructability/discord4j/Discord4JCommand.java
+++ instructability-discord4j/src/main/java/com/github/kaioru/instructability/discord4j/Discord4JCommand.java
@@ -28,6 +28,18 @@ public interface Discord4JCommand extends Command, Discord4JCommandExecutable {
 			if (event.getMessage().getChannel().isPrivate())
 				return;
 
+		if (getPermission().isPresent()) {
+			Discord4JCommandPermission reqPermission = getPermission().get();
+
+			if (!Discord4JInstructability.getInstance().getPermissionRegistry()
+					.getForGuild(event.getMessage().getGuild().getID())
+					.checkPermissions(event.getMessage().getAuthor(), event.getMessage().getGuild(), reqPermission)) {
+				msg.appendContent(String.format("You do not have the permission '%s' to use the command.", reqPermission.getName()));
+				msg.build();
+				return;
+			}
+		}
+
 		this.getExecutable().execute(args, event, msg);
 
 		if (removeTriggerMessage())
@@ -44,5 +56,9 @@ public interface Discord4JCommand extends Command, Discord4JCommandExecutable {
 	}
 
 	default boolean allowPrivateMessage() { return false; }
+
+	default Optional<Discord4JCommandPermission> getPermission() {
+		return Optional.empty();
+	}
 
 }
