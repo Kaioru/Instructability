7d6a8ab2532e447a84e946858dc79ef2e7eaf32e
Kaioru <keifuurawrs@gmail.com>
2016-05-08 10:18:56 +0800

    Add module data for Discord4J module loader

--- instructability-discord4j/src/main/java/com/github/kaioru/instructability/discord4j/InstructabilityModule.java
+++ instructability-discord4j/src/main/java/com/github/kaioru/instructability/discord4j/InstructabilityModule.java
@@ -1,38 +1,54 @@
 package com.github.kaioru.instructability.discord4j;
 
+import com.github.kaioru.instructability.Instructables;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.modules.IModule;
 
 public class InstructabilityModule implements IModule {
 
+	private static InstructabilityModule instance;
+
+	private IDiscordClient client;
+	private Discord4JCommandListener listener = new Discord4JCommandListener();
+
+	public static InstructabilityModule getInstance() {
+		return instance;
+	}
+
 	@Override
 	public boolean enable(IDiscordClient client) {
-		return false;
+		InstructabilityModule.instance = this;
+		this.client = client;
+
+		client.getDispatcher().registerListener(listener);
+		Instructables.LOGGER.info(getName() + " Discord4J Command Listener registered");
+		return true;
 	}
 
 	@Override
 	public void disable() {
-
+		client.getDispatcher().unregisterListener(listener);
+		Instructables.LOGGER.info(getName() + " Discord4J Command Listener unregistered");
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return getClass().getPackage().getImplementationTitle();
 	}
 
 	@Override
 	public String getAuthor() {
-		return null;
+		return "Kaioru";
 	}
 
 	@Override
 	public String getVersion() {
-		return null;
+		return getClass().getPackage().getImplementationVersion();
 	}
 
 	@Override
 	public String getMinimumDiscord4JVersion() {
-		return null;
+		return "2.4.6";
 	}
 
 }
